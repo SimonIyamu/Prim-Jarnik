9f51b6d5656d01621d50dfb66b16c1bd99cb08b3
Simon Iyamu <simonlflips@gmail.com>
2018-03-24 22:34:03 +0200

    Update q8.c

--- q8.c
+++ q8.c
@@ -2,9 +2,7 @@
 #include "findMST.h"
 
 int main(void){
-
-	/*This matrix is of the graph shown in the page 14 in the pdf of Unit 13*/ 
-	/*Its type is double because we want to store 1.0/0.0 */
+	/*It is of type double because we want to store 1.0/0.0 */
 	double G[N][N]= {	{0,2,8,1.0/0.0,7,1.0/0.0} , {2,0,5,7,1.0/0.0,1.0/0.0} , {8,5,0,9,8,1.0/0.0} , {1.0/0.0,7,9,0,1.0/0.0,4} , {7,1.0/0.0,8,1.0/0.0,0,3} , {1.0/0.0,1.0/0.0,1.0/0.0,4,3,0} };
 
 	FindMST(G);
